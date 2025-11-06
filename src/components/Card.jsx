d665481884a9b2900640a51bf8d6157d93f525ca
Luca Maggio <lucamaggio1992@gmail.com>
2025-11-06 11:45:20 +0100

    feat(src/components/Card): pass title and imgUrl according to react best practice

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
-function Card(props) {
+function Card({title, imgUrl}) {
     const description = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores molestiae porro nesciunt sunt, soluta totam facilis corporis voluptatum error aperiam vitae sapiente harum doloribus quae molestias! Accusantium doloremque voluptatum eaque."
 
     return (
         <div className="rounded-md bg-zinc-950">
             <img 
-                src={props.imgUrl} alt="">    
+                src={imgUrl} alt="">    
             </img>
             
             <div className="flex flex-col p-5">
                 <h2 className="text-amber-100 font-bold">
-                    {props.title}
+                    {title}
                 </h2>
                 <p className="text-gray-500">
                     {description}
