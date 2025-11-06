8d8cc4356142315faaae72ce47509da9cc715a86
Luca Maggio <lucamaggio1992@gmail.com>
2025-11-06 12:46:44 +0100

    feat(src/App.jsx): setup another city

--- src/App.jsx
+++ src/App.jsx
@@ -9,40 +9,50 @@ function App() {
     {
       id: 0,
       name: "Amsterdam",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-      imgUrl: "https://images.unsplash.com/photo-1512470876302-972faa2aa9a4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1470", 
+      description: "Charming canals, narrow houses, and a relaxed city vibe by the water.",
+      imgUrl: "https://images.unsplash.com/photo-1512470876302-972faa2aa9a4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1470",
       isVisited: true
     },
     {
       id: 1,
       name: "Rome",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+      description: "Ancient ruins, lively streets, and iconic landmarks in every direction.",
       imgUrl: "https://images.unsplash.com/photo-1548585742-1df49e753a83?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1754",
       isVisited: true
     },
     {
       id: 2,
       name: "Singapore",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-      imgUrl: "https://plus.unsplash.com/premium_photo-1697730373939-3ebcaa9d295e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1740", 
+      description: "Futuristic skyline, lush gardens, and a vibrant mix of cultures.",
+      imgUrl: "https://plus.unsplash.com/premium_photo-1697730373939-3ebcaa9d295e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1740",
       isVisited: true
     },
     {
       id: 3,
       name: "Rovaniemi",
-      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-      imgUrl: "https://images.unsplash.com/photo-1584380029866-f03d863766a5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1740", 
+      description: "Arctic town of snowy forests, husky rides, and Northern Lights hunts.",
+      imgUrl: "https://images.unsplash.com/photo-1584380029866-f03d863766a5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1740",
+      isVisited: false
+    },
+    {
+      id: 4,
+      name: "Reykjavík",
+      description: "Colorful coastal city, geothermal vibes, and gateway to Iceland’s wild nature.",
+      imgUrl: "https://images.unsplash.com/photo-1465353471565-b77e538f34c9?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1740",
       isVisited: false
     }
+    
   ]
 
   return (
     <>
     <div className='grid grid-cols-4 gap-5'>
     {
-      cities.map((city) => (
+      cities
+      //.filter((city) => !city.isVisited)
+      .map((city) => (
         <Card
-          id={city.id}
+          key={city.id}
           name={city.name}
           imgUrl={city.imgUrl}
           isVisited={city.isVisited}
